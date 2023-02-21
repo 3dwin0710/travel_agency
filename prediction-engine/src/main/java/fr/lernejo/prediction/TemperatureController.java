45a9e0e528063da936af8ab36b569fb1b801a38f
Edwin LI <eli@et.esiea.fr>
2023-02-21 18:20:07 +0100

    Added code partition for 2-day temperature prediction

--- prediction-engine/src/main/java/fr/lernejo/prediction/TemperatureController.java
+++ prediction-engine/src/main/java/fr/lernejo/prediction/TemperatureController.java
@@ -1,8 +1,39 @@
 package fr.lernejo.prediction;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
 public class TemperatureController {
+    private final List<String> list_of_data = new ArrayList<>();
+    private final long Date_Today =System.currentTimeMillis();
+    private final long Date_Yesterday = System.currentTimeMillis()- 1000L * 60L * 60L * 24L;
+
+
+    public String newFeatures(String Pays, List<String> list){
+        String newFeatures = "{\n\t\"country\": \""+Pays+"\",\n\t\"temperatures\": "+list+"\n\t \n}";
+        return newFeatures;
+
+    }
+    @GetMapping("/api/temperature")
+    public String getCountry(@RequestParam String country)
+    {
+        java.sql.Date date = new java.sql.Date(Date_Today);
+        java.sql.Date yesterday =  new java.sql.Date(Date_Yesterday);
+        TemperatureService service = new TemperatureService();
+        String prefeature = "\n\t\t{\n\t\t\t\"date\":\""+date+"\",\n\t\t\t\"temperature\": "+service.getTemperature(country)+"\n\t\t}";
+        String prefeaturas = "\n\t\t{\n\t\t\t\"date\":\""+yesterday+"\",\n\t\t\t\"temperature\": "+service.getTemperature(country)+"\n\t\t}\n\n\t";
+        list_of_data.add(prefeature);
+        list_of_data.add(prefeaturas);
+        return newFeatures(country,list_of_data);
+    }
+
+
 
 
 }
